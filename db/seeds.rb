780ed853df7d55374efd9ec2473387fad77fc8a5
Noel Delgado <noeldelgadom@myhost.local>
2017-06-30 12:09:00 -0500

    added index action to items

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 10.times do |blog|
   Blog.create!(
-    title: 'Blog Post #{blog}',
+    title: 'Blog Post ' + blog.to_s,
     body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
   )
 end
@@ -18,7 +18,7 @@ puts '5 skills created'
 
 9.times do |portfolio_item|
   Portfolio.create!(
-    title: 'Portfolio Item #{portfolio_item}',
+    title: 'Portfolio Item ' + portfolio_item.to_s,
     subtitle: 'My greate service',
     body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
     main_image: 'https://placeholdit.co//i/600x400',
