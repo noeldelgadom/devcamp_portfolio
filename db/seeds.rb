c2c33d23b9122299309bab86f4ca2bc90fb42bba
Noel Delgado <noeldelgadom@myhost.local>
2017-07-03 12:17:47 -0500

    implemented custom scopes for portoflio items

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,16 @@
+3.times do |topic|
+  Topic.create!(
+    title: 'Topic ' + topic.to_s
+  )
+end
+
+puts '3 topics created'
+
 10.times do |blog|
   Blog.create!(
     title: 'Blog Post ' + blog.to_s,
     body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    topic_id: Topic.last.id
   )
 end
 
@@ -16,14 +25,22 @@ end
 
 puts '5 skills created'
 
-9.times do |portfolio_item|
+8.times do |portfolio_item|
   Portfolio.create!(
     title: 'Portfolio Item ' + portfolio_item.to_s,
-    subtitle: 'My greate service',
+    subtitle: 'Ruby',
     body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
     main_image: 'https://placeholdit.co//i/600x400',
     thumb_image: 'https://placeholdit.co//i/350x200',
   )
 end
+
+Portfolio.create!(
+  title: 'Angular Item',
+  subtitle: 'Anuglar',
+  body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+  main_image: 'https://placeholdit.co//i/600x400',
+  thumb_image: 'https://placeholdit.co//i/350x200',
+)
 
 puts '9 portfolios created'
