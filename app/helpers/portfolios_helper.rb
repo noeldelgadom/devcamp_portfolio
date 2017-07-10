771113e9147999eba3470bcc51e8403afa5ff80c
Noel Delgado <noeldelgadom@myhost.local>
2017-07-10 16:26:58 -0500

    updated file upload forms and refactored placeholder functionality into view

--- app/helpers/portfolios_helper.rb
+++ app/helpers/portfolios_helper.rb
@@ -1,2 +1,15 @@
 module PortfoliosHelper
+  def image_generator(height:, width:)
+    'https://placeholdit.co//i/' + height.to_s + 'x' + width.to_s
+  end
+
+  def portfolio_img img, type
+    if img.model.main_image? || img.model.thumb_image?
+      img
+    elsif type == 'thumb'
+      image_generator(height: 350, width: 200)
+    else
+      image_generator(height: 600, width: 400)
+    end
+  end
 end
