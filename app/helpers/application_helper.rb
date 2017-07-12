4d5339ed417ed766a5aa4227cf356e9c241a6fd8
Noel Delgado <noeldelgadom@myhost.local>
2017-07-12 12:11:55 -0500

    refactored alerts into helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -59,4 +59,12 @@ module ApplicationHelper
     "active" if current_page? path
   end
 
+  def alerts
+    alert = (flash[:error] || flash[:alert] || flash[:notice])
+    alert_generator alert if alert
+  end
+
+  def alert_generator msg
+    js add_gritter(msg, title: "Jordan Hudgens Portfolio") 
+  end
 end
