bb3906603ba036d9050067898129805fdb53b5b9
Noel Delgado <noeldelgadom@myhost.local>
2017-07-12 15:29:50 -0500

    styled tech news page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -42,6 +42,10 @@ module ApplicationHelper
         url: portfolios_path,
         title: 'Portfolio'
       },
+      {
+        url: tech_news_path,
+        title: 'Tech News'
+      },
     ]
   end
 
@@ -65,6 +69,6 @@ module ApplicationHelper
   end
 
   def alert_generator msg
-    js add_gritter(msg, title: "Jordan Hudgens Portfolio") 
+    js add_gritter(msg, title: "Jordan Hudgens Portfolio")
   end
 end
